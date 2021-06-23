0c6390c988858ee1f46230ef243d3f55f15a9e24
dxu <dean.xu@asml.com>
2021-06-23 08:50:05 +0800

    fix redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,13 @@
-import {GetStaticProps} from 'next'
 import {AllArticles} from "../components/article/article";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 export default function Index() {
+  const router = useRouter();
+  useEffect(() => {
+    const articleSet = AllArticles[0]
+    const article = articleSet.articles[0]
+    router.replace(`/article/spark/${articleSet.id}/${article.meta.id}`)
+  })
   return null
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const articleSet = AllArticles[0]
-  const article = articleSet.articles[0]
-  return {
-    redirect: {
-      destination: `/article/spark/${articleSet.id}/${article.meta.id}`,
-      permanent: false,
-    },
-  }
 }
